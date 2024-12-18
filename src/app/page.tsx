adb51ae0c52886d5d2062f18895bdb48620414e3
Roekpasawat Sajjapan <75149104+Lockps@users.noreply.github.com>
2024-12-18 12:15:20 +0700

    understand tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>asd</h1>;
+  return (
+    <div className="flex gap-10 bg-sky-800 justify-around items-center hover:bg-sky-50 ease-in duration-300">
+      <div className="bg-sky-100">a</div>
+      <div className="bg-sky-100">a</div>
+      <div className="bg-sky-100">a</div>
+      <div className="bg-sky-100">a</div>
+    </div>
+  );
 }
